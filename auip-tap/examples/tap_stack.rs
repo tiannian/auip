9ae8f08072c1bcbb10bd5ff1d9c9d99dbe5fbaec
tiannian <dtiannian@aliyun.com>
2021-01-24 00:02:59 +0800

    add ip for tap0

--- auip-tap/examples/tap_stack.rs
+++ auip-tap/examples/tap_stack.rs
@@ -6,11 +6,16 @@ use tokio::process::Command;
 #[tokio::main]
 async fn main() {
     let mut file = open_tap_device("tap0").await.unwrap();
-    let mut buffer = [0u8; 128];
-    file.read(&mut buffer).await.unwrap();
     let mut command = Command::new("ip")
-        .arg("addr add 192.168.69.100/24 dev tap0")
+        .arg("addr")
+        .arg("add")
+        .arg("192.168.69.100/24")
+        .arg("dev")
+        .arg("tap0")
         .spawn()
         .unwrap();
-    let status = child.wait().await?;
+    let status = command.wait().await.unwrap();
+    println!("Set ip {}", status);
+    let mut buffer = [0u8; 128];
+    file.read(&mut buffer).await.unwrap();
 }
