e8d7f28b5526e9308dda5cda87130017f93fa749
tiannian <dtiannian@aliyun.com>
2021-01-29 01:35:10 +0800

    add begin of interface

--- auip/src/interface.rs
+++ auip/src/interface.rs
@@ -1,21 +1,30 @@
 //! interface layer.
 
 // use crate::phy::Device;
-// use auip_pkt::ip;
-// use managed::ManagedSlice;
+use auip_pkt::{ip, mac};
+use managed::ManagedSlice;
 
-// pub struct Interface<'a> {
-//     name: &'a str,
-//     // device: &'a DeviceT,
-//     address: ManagedSlice<'a, ip::Address>,
-// }
+pub struct Interface<'a> {
+    name: &'a str,
+    // device: &'a DeviceT,
+    // ip_address: ManagedSlice<'a, ip::Address>,
+    // mac_address: mac::Address,
+}
 
-// impl<'a> Interface<'a> {
-//     pub fn new(name: &'a str) -> Self {
-//         Self {
-//             name,
-//             // device,
-//             address: ManagedSlice::Borrowed(&mut[])
-//         }
-//     }
-// }
+impl<'a> Interface<'a> {
+    pub fn new(name: &'a str) -> Self {
+        Self {
+            name,
+            // device,
+            // address: ManagedSlice::Borrowed(&mut[])
+        }
+    }
+
+    pub async fn loop_arp(&mut self) {
+
+    }
+
+    pub async fn loop_icmp(&mut self) {
+
+    }
+}
