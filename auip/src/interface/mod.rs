4ddc313726c9d6366325c273a068123823b56069
tiannian <dtiannian@gmail.com>
2022-10-22 03:59:59 +0800

    add vlan config

--- auip/src/interface/mod.rs
+++ auip/src/interface/mod.rs
@@ -1,6 +1,9 @@
 mod iface;
 pub use iface::*;
 
+mod vlan;
+pub use vlan::*;
+
 pub mod action;
 
 pub(crate) mod utils;
--- auip/src/lib.rs
+++ auip/src/lib.rs
@@ -13,7 +13,7 @@ mod device;
 pub use device::*;
 
 pub mod interface;
-pub use interface::Interface;
+pub use interface::{Interface, Vlan};
 
 pub mod storage;
 
