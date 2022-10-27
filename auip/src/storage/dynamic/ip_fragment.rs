7479f968512e7bc4f9bf47acaf926660518ab722
tiannian <dtiannian@gmail.com>
2022-10-28 01:28:08 +0800

    fix dyn ip frag

--- auip/src/storage/dynamic/ip_fragment.rs
+++ auip/src/storage/dynamic/ip_fragment.rs
@@ -1,10 +1,10 @@
 use alloc::vec::Vec;
 
-use crate::{consts::MAX_IP_FRAGMENT_PACKET_LENGTH, utils::FixedBytes, IpFragmentBuffer};
+use crate::{consts::MAX_IP_FRAGMENT_PACKET_LENGTH, IpFragmentBuffer};
 
 pub struct IpFragment {
     pub max_length: usize,
-    pub buffers: Vec<FixedBytes<{ MAX_IP_FRAGMENT_PACKET_LENGTH }>>,
+    pub buffers: Vec<u8>,
 }
 
 impl IpFragment {
@@ -24,7 +24,14 @@ impl IpFragmentBuffer for IpFragment {
 
     fn get_buffer(&mut self, idx: usize) -> Option<&mut [u8]> {
         if idx < self.max_length {
-            None
+            let end = (idx + 1) * MAX_IP_FRAGMENT_PACKET_LENGTH;
+            let begin = idx * MAX_IP_FRAGMENT_PACKET_LENGTH;
+
+            if end > self.buffers.len() {
+                self.buffers.resize(end, 0);
+            }
+
+            Some(&mut self.buffers[begin..end])
         } else {
             None
         }
